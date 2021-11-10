c51581d4a156372f79b6c1743af9b3a25df05cb9
truongbanghia <truongbanghia@gmail.com>
2021-11-10 22:11:17 +0700

    change audio

--- src/main.js
+++ src/main.js
@@ -16,9 +16,8 @@ var game = new Vue({
     seen: false,
     remaining: 100,
     cursorImage: "figs/hakkekyu.png",
-    // virusImage に 変更したいです _kugi_
-    image: "figs/Profile_UNI7694.JPG",
-    bgm: new Audio("sounds/BGM.mp3"),
+    image: "figs/boss.png",
+    bgm: new Audio("figs/Astronomia.mp3"),
     times: [],
     animateFrame: 0,
     nowTime: 0,
@@ -30,7 +29,6 @@ var game = new Vue({
     isResult: false,
   },
   methods:{
-    // ウイルスの残り数を更新する
     decrementTotalCount: function(){
       var sound = new Audio("sounds/deleteVirusSE.mp3");
       sound.volume = 0.5;
@@ -48,22 +46,17 @@ var game = new Vue({
       this.isTitle = false;
       gameStart();
     },
-    //リスタート
     restart: function(){
       game.seen = false;
       sleep(1, gameStart);
     },
-    // 現在時刻から引数に渡した数値を startTime に代入
     setSubtractStartTime: function (time) {
       var time = typeof time !== 'undefined' ? time : 0;
       this.startTime = Math.floor(performance.now() - time);
     },
-    // タイマーをスタートさせる
     startTimer: function () {
-      // loop()内で this の値が変更されるので退避
       var vm = this;
       vm.setSubtractStartTime(vm.diffTime);
-      // ループ処理
       (function loop(){
         vm.nowTime = Math.floor(performance.now());
         vm.diffTime = vm.nowTime - vm.startTime;
@@ -71,12 +64,10 @@ var game = new Vue({
       }());
       vm.isRunning = true;
     },
-    // タイマーを停止させる
     stopTimer: function () {
       this.isRunning = false;
       cancelAnimationFrame(this.animateFrame);
     },
-    // 計測中の時間を配列に追加
     pushTime: function () {
       this.times.push({
         hours: this.hours,
@@ -85,7 +76,6 @@ var game = new Vue({
         milliSeconds: this.milliSeconds
       });
     },
-    // 初期化
     clearAll: function () {
       this.startTime = 0;
       this.nowTime = 0;
@@ -96,33 +86,27 @@ var game = new Vue({
     }
   },
   computed: {
-    // 分数を計算 (60分になったら0分に戻る)
     minutes: function () {
       return Math.floor(this.diffTime / 1000 / 60) % 60;
     },
-    // 秒数を計算 (60秒になったら0秒に戻る)
     seconds: function () {
       return Math.floor(this.diffTime / 1000) % 60;
     },
-    // ミリ数を計算 (1000ミリ秒になったら0ミリ秒に戻る)
     milliSeconds: function () {
       return Math.floor(this.diffTime % 1000);
     },
-    // 分数を計算 (60分になったら0分に戻る)
     storageMinutes: function () {
       if(!this.highScore){
         return "99";
       }
       return Math.floor(this.highScore / 1000 / 60) % 60;
     },
-    // 秒数を計算 (60秒になったら0秒に戻る)
     storageSeconds: function () {
       if(!this.highScore){
         return "99";
       }
       return Math.floor(this.highScore / 1000) % 60;
     },
-    // ミリ数を計算 (1000ミリ秒になったら0ミリ秒に戻る)
     storageMilliSeconds: function () {
       if(!this.highScore){
         return "999";
@@ -131,8 +115,6 @@ var game = new Vue({
     },
   },
   filters: {
-    // ゼロ埋めフィルタ 引数に桁数を入力する
-    // ※ String.prototype.padStart() は IEじゃ使えない
     zeroPad: function(value, num){
       var num = typeof num !== 'undefined' ? num : 2;
       return value.toString().padStart(num,"0");
@@ -142,11 +124,11 @@ var game = new Vue({
 
 function gameStart(){
   game.bgm.loop = true;
-  game.bgm.volume = 0.3;
+  game.bgm.volume = 0.5;
   game.bgm.play();
   title.seen = false;
   game.isResult = false;
-  game.remaining = 100;
+  game.remaining = 200;
   game.seen = true;
   saveData();
   game.clearAll();
@@ -166,16 +148,11 @@ function showResult(){
 }
 
 function sleep(waitSec, callbackFunc) {
-  // 経過時間（秒）
   var spanedSec = 0;
-  // 1秒間隔で無名関数を実行
   var id = setInterval(function () {
       spanedSec++;
-      // 経過時間 >= 待機時間の場合、待機終了。
       if (spanedSec >= waitSec) {
-          // タイマー停止
           clearInterval(id);
-          // 完了時、コールバック関数を実行
           if (callbackFunc) callbackFunc();
       }
   }, 100);
